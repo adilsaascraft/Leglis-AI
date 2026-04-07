23063b632404cd06a39e10bdc400fe8eb8c40a65
adilsaascraft <adilsaascraft@gmail.com>
2026-04-08 00:28:24 +0530

    config file updated

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,11 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'aig-academics.s3.ap-southeast-1.amazonaws.com',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com', pathname: '/**' },
+      { protocol: 'https', hostname: 'aig-academics.s3.ap-southeast-1.amazonaws.com', pathname: '/**' },
     ],
   },
-  webpack(config) {
-    // Add SVGR support for importing SVGs as React components
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
+  turbopack: {}, // enable empty turbopack config
 }
 
 export default nextConfig
